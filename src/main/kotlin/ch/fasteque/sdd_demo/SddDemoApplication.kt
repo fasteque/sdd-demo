8ca2e5f95aca1e37683e1786a0cdd933977df821
fasteque <daniele.altomare@gmail.com>
2026-07-03 15:50:44 +0200

    Initial Spring Boot + Kotlin + MongoDB skeleton

---
+++ src/main/kotlin/ch/fasteque/sdd_demo/SddDemoApplication.kt
@@ -0,0 +1,11 @@
+package ch.fasteque.sdd_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SddDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SddDemoApplication>(*args)
+}
---
+++ src/test/kotlin/ch/fasteque/sdd_demo/SddDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.fasteque.sdd_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SddDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
